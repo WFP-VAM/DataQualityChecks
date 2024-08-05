5b89d9ad65087ad902543954bf2e824a8d5c4e22
GHERARDELLI Alessandra <alessandra.gherardelli@gmail.com>
2024-08-05 14:35:29 +0200

    Updated DataBridges IDs for DRC v2

--- data_bridges_config.py
+++ data_bridges_config.py
@@ -1,7 +1,9 @@
 DATA_BRIDGES_CONFIG = {
-    "questionnaire_id": 1737,
-    "survey_id": 3607,   
+    # "questionnaire_id": 1737,# OLD
+    # "survey_id": 3607,   # OLD
+    "questionnaire_id": 1740,
+    "survey_id": 3653, 
     "country_name": "DRC",
     "credentials_file_path": r"databridges_api_database_credentials.yaml",
-    "data_file_extract": r"data/drc_efsa_data.csv"
+    "data_file_extract": r"data/drc_efsa_data.csv" # BACKUP IF PIPELINE FAILS
 }
